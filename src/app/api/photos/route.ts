feaf1db39158b19240c416544c1587351af4db40
Yoji Shidara <dara@shidara.net>
2023-05-15 17:38:11 +0900

    Works

---
+++ src/app/api/photos/route.ts
@@ -0,0 +1,19 @@
+import fs from "node:fs/promises";
+import YAML from "yaml";
+
+export type Photo = {
+  url: string;
+};
+
+export async function GET(request: Request) {
+  let photos: Photo[] = [];
+  const dataDir = "./data";
+  const files = await fs.readdir(dataDir);
+  for (const file of files) {
+    const yaml = await fs.readFile(`${dataDir}/${file}`, "utf-8");
+    const photo = YAML.parse(yaml);
+    photos.push(photo);
+  }
+
+  return new Response(JSON.stringify({ photos }), { status: 200 });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Photo } from "./api/photos/route";
+
+async function getPhotos() {
+  const res = await fetch(`/api/photos`, {
+    next: { revalidate: 60 },
+  });
+  const data = await res.json();
+
+  return data.photos;
+}
+
 export default function Home() {
-  return <main>HELLO</main>;
+  const [photo, setPhoto] = useState<Photo>();
+
+  useEffect(() => {
+    (async () => {
+      const photos = await getPhotos();
+      setPhoto(photos[0]);
+    })();
+  });
+
+  return <main>{photo && <img src={photo.url}></img>}</main>;
 }
