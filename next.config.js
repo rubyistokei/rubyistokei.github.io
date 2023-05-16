6d052c328f04ad7b88979cdbb511c943e4a3ffd8
Yoji Shidara <dara@shidara.net>
2023-05-16 11:34:43 +0900

    Consider base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,12 @@ import { format } from "date-fns";
 import { Photo } from "./api/photos/route";
 
 async function getPhotos() {
-  const res = await fetch(`/api/photos`, {
-    next: { revalidate: 60 },
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_PATH || ""}/api/photos`,
+    {
+      next: { revalidate: 60 },
+    }
+  );
   const data = await res.json();
 
   return data.photos;
